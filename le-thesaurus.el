a5d19479ab18ded79b30c7a9b7d6194e84a03dac
Anselm Coogan <anselm.coogan@icloud.com>
2022-05-17 19:25:33 +0200

    extra semicolon in description

--- le-thesaurus.el
+++ le-thesaurus.el
@@ -20,7 +20,7 @@
 
 ;;; Commentary:
 
-;;; Uses the Elisp request library (https://github.com/tkf/emacs-request) and the thesaurus.com API to fetch synonyms
+;; Uses the Elisp request library (https://github.com/tkf/emacs-request) and the thesaurus.com API to fetch synonyms
 
 ;;; Code:
 
